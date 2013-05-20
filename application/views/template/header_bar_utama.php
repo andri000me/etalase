89a89a71a7e8ec67ce9e5d2f80b5c9483b27ed8d
gripp <oberon4mine@yahoo.com>
2013-05-20 13:50:24 +0700

    bikin tombol logo buat home

--- application/views/template/header_bar_utama.php
+++ application/views/template/header_bar_utama.php
@@ -2,7 +2,9 @@
 		<div id="header-bar">
 			<div id="header-content">
 				<div class="kiri">
-					<img src="<?php echo base_url()?>img/logo_small.png" width="20px"/>
+					<a href="<?php echo base_url()?>">
+						<img src="<?php echo base_url()?>img/logo_small.png" width="20px"/>
+					</a>
 				</div>
 				<div class="kanan">
 					<a href=<?php echo base_url()?>index.php/login/proses_login>Login</a>
