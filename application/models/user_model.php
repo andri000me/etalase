3bab88ab36c1becbfc0350a99056370386a1bd82
rahloki <m.firmansyah@student.upi.edu>
2013-06-06 09:20:29 +0700

    sudah di pake enkiripsi md5

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -5,7 +5,7 @@ class User_model extends CI_Model{
         $simpan_data=array(
             'nama_lengkap'  => $this->input->post('nama_lengkap'),
             'username'      => $this->input->post('username'),
-            'password'      => $this->input->post('password'),
+            'password'      => md5($this->input->post('password')),
             'email'         => $this->input->post('email'),
        );
         $simpan = $this->db->insert('user', $simpan_data);
@@ -14,12 +14,14 @@ class User_model extends CI_Model{
 	
 	//cek username dan password untuk login
 	function cek_username_password($username, $password){
-		$query = $this->db->get_where('user', array('username' => $username, 'password' => $password));
-		if($query->num_rows()== 1){
-			return true;
-		}else{
-			return false;
-		}
+		$this->db->where('username', $this->input->post('username'));
+            $this->db->where('password', md5($this->input->post('password')));
+            $query = $this->db->get('user');
+ 
+            if($query->num_rows == 1)
+            {
+                return TRUE;
+            }
 	}
 
 	function get_id_by_username($username){
